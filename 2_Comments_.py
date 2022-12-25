911b495ee8b6a3141b0dbc5059100e5cc603da48
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-12-25 12:13:31 +0530

    Add files via upload

---
+++ 2_Comments_.py
@@ -0,0 +1,14 @@
+#Comments, Escape Sequences(\) & Print Statement
+
+
+# Hii this is Comment line.
+print("Shubham Gite")
+print("i am \n Omprasad")
+print("i am \"Shubham\" Gite")
+
+'''
+Hii this is multi line Comment 
+My name is Comment
+'''
+print("harry",8,7,sep="~", end="harryDada\n")
+print("Jaali")
