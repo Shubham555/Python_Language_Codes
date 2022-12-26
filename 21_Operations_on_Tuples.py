e7382a81c825ab4aed0ecc05c84905f00af0f42b
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-12-27 01:51:36 +0530

    Add files via upload

---
+++ 21_Operations_on_Tuples.py
@@ -0,0 +1,9 @@
+tuple1 = (0, 1, 2, 3, 2, 311, 1, 3, 2, 3)
+res = tuple1.count(3)
+# res = tuple1.index(3)
+# res = tuple1.index(311)
+# res = tuple1.index(3, 4, 8)
+print('Count of 3 in tuple1 is:', res)
+
+res = len(tuple1)
+print('length of tuple1 is:', res)
