868ddc910dd126ade4a6fde308f8d681c9d0f8f9
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-12-30 19:02:46 +0530

    Add files via upload

---
+++ 25_Sets.py
@@ -0,0 +1,11 @@
+s = {2,4,6,2}  # it doesnot maintain order while printing also
+print(s)
+
+info = {"Carla",19,False,5.9,19}
+print(info)
+
+harry = set()
+print(type(harry))
+
+for value in info:
+  print(value)
